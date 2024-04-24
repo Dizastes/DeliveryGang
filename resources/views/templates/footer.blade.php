82c401bf6a262cb4e17cc5662ac9101d66cc181c
Inkashi <zabolotin.nikitka@mail.ru>
2024-04-24 16:54:18 +0500

    пред.ком

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,44 @@
+<footer class="m-footer">
+    <div class="footer-content container text-center">
+        <div class="row">
+            <div class="col-4 info" style="text-align: start">
+                <!-- Первое поле -->
+                <p>При поддержке</p>
+                <p> Югорского<br>
+                    Государственного<br> 
+                    Университета
+                </p>
+                <p>
+                    Томского<br> 
+                    Государственного<br> 
+                    Университета<br>
+                </p>
+            </div>
+            <div class="col-4 info">
+                <!-- Второе поле -->
+                <p>Доставка осуществляется<br>
+                    с 9:00 до 23:00<br>
+                    без выходных
+                </p>
+                <p>Контактные данные:<br>
+                    тел. +7 800 555 35 35<br>
+                    e-mail: gang@ugra.su
+                </p>
+                <p>Адрес: <br>
+                    г. Ханты-Мансийск, ул Чехова д.16
+                </p>
+                <p>2024 г.</p>
+            </div>
+            <div class="col-4 info" style="text-align: end">
+                <p>Отдел<br>
+                    контроля качества</p>
+                <p>Валерий<br>
+                    +7 666 666 66 66</p>
+                <p>Рина<br>     
+                    +7 123 456 78 90</p>
+                <p>Денис<br>
+                    +7 032 382 92 57</p>
+            </div>
+        </div>
+    </div>
+</footer>
