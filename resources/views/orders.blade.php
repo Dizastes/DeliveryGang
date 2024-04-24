8707fb48e4a5f2ccdf59a9ee7af99caa742c38e4
Michail Maksimchuk <mishamx04@gmail.com>
2024-04-24 22:35:45 +0500

    add header template

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -4,20 +4,11 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>DeliveryGang</title>
+	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 	<script src="https://js.pusher.com/7.0/pusher.min.js"></script>
 </head>
 <body>
-	<h1>Главная</h1>
-	<div class="header"></div> <!-- блок для header -->
-	<div class="adv"></div> <!-- блок для рекламы -->
-	<div class="main_block"> <!-- основной блок с товарами -->
-		<!-- <div class="item">
-            @foreach ($items as $item)
-                <div class="item">{{ $item->status}} человек под {{ $item->id }} будет через {{ $item->time }}</div>
-            @endforeach
-		</div> -->
-	</div>
-	<div class="footer"></div> <!-- блок для footer -->
+	@include('templates.header')
 	@php
 		$string = 'templates.' . $form_name . '_form';
 	@endphp
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,33 @@
+<div class="header_area">
+	<div class="header_block">
+		<div class="logo_area">
+			<img src="{{ asset('images/logo.png') }}" class="order_logo">
+		</div>
+		<div class="name_area">
+			<div class="name_position">
+				<div class="titile">
+					<h3>Delivery Gang</h3>
+				</div>
+				<div class="subtitle">
+					<p>courier</p>
+				</div>
+			</div>
+		</div>
+		<div class="button_area">
+			<div class="menu_button">
+				<div class="line"></div>
+				<div class="line"></div>
+				<div class="line"></div>
+			</div>
+			<div class="profile_button">
+				<img src="{{ asset('images/human.png') }}" class="profile_img">
+			</div>
+		</div>
+	</div>
+	<div class="order_line_block">
+		<div class="text">Заказы</div>
+		<div class="line_block">
+			<div class="order_line"></div>
+		</div>
+	</div>
+</div>
